acabc1c493fdbba651c978263743c79fd8181968
zhyk <2697516802@qq.com>
2022-03-25 19:57:08 +0800

    第十一关

--- thumbnail.js
+++ thumbnail.js
@@ -3,7 +3,5 @@ targetImage.addEventListener("click", function() {
 
     if (targetImage.classList.contains("small")) {
         targetImage.classList.remove("small");
-    } else {
-        targetImage.classList.add("small");
     }
 })
