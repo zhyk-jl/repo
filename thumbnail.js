01420ec696798512a1059e5340b23fdaa95f0e7e
zhyk <2697516802@qq.com>
2022-03-25 19:55:50 +0800

    第十一关

---
+++ thumbnail.js
@@ -0,0 +1,9 @@
+let targetImage = document.querySelector("#smart-image")
+targetImage.addEventListener("click", function() {
+
+    if (targetImage.classList.contains("small")) {
+        targetImage.classList.remove("small");
+    } else {
+        targetImage.classList.add("small");
+    }
+})
